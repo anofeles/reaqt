a68573f486c2b237fea55370573568a51c14e5c5
kaxam1 <kaxam1@gmail.com>
2019-06-09 11:23:37 +0400

    home message

--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -1,24 +1,31 @@
 import React from 'react'
 import classes from './Dialogs.module.css'
+import {NavLink} from "react-router-dom";
+
+function DdialogItem(props) {
+    return (
+        <div className={classes.dialog}>
+            <NavLink to={'/Messages/'+props.data_id+''}> {props.data_name}</NavLink>
+        </div>
+    );
+}
 
 function Dialogs() {
     return (
-      <div className={classes.dialogs}>
-          <div className={classes.dialogs_items}>
-             <div className={classes.dialog}>dialog</div>
-             <div className={classes.dialog}>dialog</div>
-             <div className={classes.dialog}>dialog</div>
-             <div className={classes.dialog}>dialog</div>
-             <div className={classes.dialog}>dialog</div>
-             <div className={classes.dialog}>dialog</div>
-             <div className={classes.dialog}>dialog</div>
-          </div>
-          <div className={classes.messages}>
-              <div className={classes.message}>message</div>
-              <div className={classes.message}>message</div>
-              <div className={classes.message}>message</div>
-          </div>
-      </div>
+        <div className={classes.dialogs}>
+            <div className={classes.dialogs_items}>
+               <DdialogItem data_id={'1'} data_name={'kakha'}/>
+               <DdialogItem data_id={'2'} data_name={'cira'}/>
+               <DdialogItem data_id={'3'} data_name={'gabie'}/>
+               <DdialogItem data_id={'4'} data_name={'mari'}/>
+               <DdialogItem data_id={'5'} data_name={'ruso'}/>
+            </div>
+            <div className={classes.messages}>
+                <div className={classes.message}>message</div>
+                <div className={classes.message}>message</div>
+                <div className={classes.message}>message</div>
+            </div>
+        </div>
     );
 }
 
